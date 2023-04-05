993d85f755ac9b19c9c7d7193776752d2e0e57c3
zacharyplat <zachary.plat@gmail.com>
2023-04-05 11:13:17 -0700

    fixed some bugs and styled the list items

--- src/List/components/List.tsx
+++ src/List/components/List.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, TextInput, View } from 'react-native';
+import { Button, StyleSheet, TextInput, View } from 'react-native';
 import ListItem from './ListItem';
 
 type ListItem = {
@@ -8,11 +8,12 @@ type ListItem = {
   isCompleted: boolean;
 };
 function randId() {
-  return Math.floor(Math.random() * 10 ** 5) + '';
+  return Date.now() + '';
 }
 export default function List() {
-  const [text, onChangeText] = useState('');
+  const [firstText, setFirstText] = useState('');
   const [list, setList] = useState<ListItem[]>([]);
+  const [focusedItem, setFocusedItem] = useState('');
 
   const addFirstItem = (text: string) => {
     const newItem: ListItem = {
@@ -21,6 +22,9 @@ export default function List() {
       isCompleted: false,
     };
     setList([newItem]);
+    setFirstText('');
+    setFocusedItem(newItem.id);
+    console.log('addFirstItem');
   };
 
   const editListItem = (text: string, id: string) => {
@@ -28,9 +32,10 @@ export default function List() {
       item.id === id ? { ...item, text } : item
     );
     setList(editedList);
+    console.log('editListItem');
   };
 
-  const addItemToList = (id?: string) => {
+  const appendItemToList = (id?: string) => {
     const newItem: ListItem = {
       id: randId(),
       text: '',
@@ -39,36 +44,51 @@ export default function List() {
     const newList = list.flatMap(
       (item) => (item.id === id && [item, newItem]) || item
     );
-    console.log(list);
     id ? setList(newList) : setList([newItem]);
+    setFocusedItem(newItem.id);
+    console.log('appendItemToList');
   };
 
   const removeItem = (id: string) => {
+    const previousId = getPreviousId(id);
+    console.log(`old focusid ${focusedItem}`);
+    console.log(`new focusid ${previousId}`);
+    setFocusedItem(previousId);
     const newList = list.filter((item) => item.id !== id);
     setList(newList);
+    console.log('removeItem');
+  };
+  const getPreviousId = (id: string) => {
+    console.log('getpreviousId');
+    const idIndex = list.findIndex((item) => item.id === id);
+    return list[idIndex - 1].id;
   };
 
   return (
     <View style={styles.container}>
       {list.map((item) => (
-        <ListItem
-          id={item.id}
-          key={item.id}
-          text={item.text}
-          addItemToList={addItemToList}
-          editListItem={editListItem}
-          removeItem={removeItem}
-        />
+        <View style={styles.listItem} key={item.id}>
+          <ListItem
+            id={item.id}
+            key={item.id}
+            text={item.text}
+            focused={item.id === focusedItem}
+            appendItemToList={appendItemToList}
+            editListItem={editListItem}
+            removeItem={removeItem}
+          />
+        </View>
       ))}
-      {!list.length && (
+      <View style={styles.listItem}>
+        <Button title="[+]" onPress={() => addFirstItem(firstText)} />
         <TextInput
           style={styles.input}
-          onChangeText={onChangeText}
-          value={text}
-          placeholder="Add item"
-          onSubmitEditing={() => addFirstItem(text)}
+          onChangeText={setFirstText}
+          value={firstText}
+          placeholder="List item"
+          onSubmitEditing={() => addFirstItem(firstText)}
         />
-      )}
+      </View>
     </View>
   );
 }
@@ -85,10 +105,15 @@ const styles = StyleSheet.create({
     fontSize: 28,
     fontWeight: 'bold',
   },
+  listItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin: 10,
+  },
   input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
+    flexShrink: 0,
+    flexGrow: 1,
+    borderBottomColor: 'grey',
+    borderBottomWidth: 2,
   },
 });
--- src/List/components/ListItem.tsx
+++ src/List/components/ListItem.tsx
@@ -1,39 +1,46 @@
 import Checkbox from 'expo-checkbox';
 import * as React from 'react';
-import { Button, StyleSheet, TextInput, View } from 'react-native';
+import { StyleSheet, TextInput, View } from 'react-native';
 
 type Props = {
   id: string;
   text: string;
-  addItemToList: (id?: string) => void;
+  focused: boolean;
+  appendItemToList: (id?: string) => void;
   editListItem: (text: string, id: string) => void;
-  removeItem: (text: string) => void;
+  removeItem: (id: string) => void;
 };
 
 export default function ListItem(props: Props) {
-  const { id, text, addItemToList, editListItem, removeItem } = props;
+  const { id, text, focused, appendItemToList, editListItem, removeItem } =
+    props;
   const [state, setState] = React.useState(false);
-  const [currentText, setCurrentText] = React.useState('');
   const handleChangeText = (string: string) => {
     editListItem(string, id);
-    setCurrentText(string);
   };
-  const handleEndEditing = () => {
-    if (!currentText) {
+  const handleOnKeyPress = (event: { nativeEvent: { key: string } }) => {
+    console.log(event.nativeEvent.key);
+    if (!text && event.nativeEvent.key === 'Backspace') {
       removeItem(id);
     }
   };
+  console.log(`item ${id} focused: ${focused}`);
   return (
     <View style={styles.container}>
-      <Checkbox value={state} onValueChange={setState} />
+      <Checkbox
+        style={styles.checkbox}
+        value={state}
+        onValueChange={setState}
+      />
       <TextInput
-        style={styles.title}
+        style={[styles.item, styles.input]}
         onChangeText={handleChangeText}
-        onEndEditing={handleEndEditing}
+        onKeyPress={handleOnKeyPress}
+        onSubmitEditing={() => appendItemToList(id)}
+        autoFocus={focused}
       >
         {text}
       </TextInput>
-      <Button title="[+]" onPress={() => addItemToList(id)} />
     </View>
   );
 }
@@ -43,8 +50,16 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
   },
-  title: {
-    fontSize: 28,
-    fontWeight: 'bold',
+  checkbox: {
+    marginRight: 18,
+  },
+  item: {
+    fontSize: 18,
+  },
+  input: {
+    flexShrink: 0,
+    flexGrow: 1,
+    borderBottomColor: 'darkgrey',
+    borderBottomWidth: 2,
   },
 });
